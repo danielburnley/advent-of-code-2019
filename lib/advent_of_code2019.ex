d8d05d49408073828d78c3e5681b075ba19f90b6
Daniel Burnley <dan@danops.uk>
2019-12-03 20:42:02 +0000

    Refactor main

--- lib/advent_of_code2019.ex
+++ lib/advent_of_code2019.ex
@@ -13,16 +13,20 @@ defmodule AdventOfCode2019 do
 
   """
   def run do
-    day_one_part_one =
-      File.read!("lib/inputs/1.txt")
-      |> String.trim()
-      |> String.split("\n")
+    day_one =
+      file_to_inputs(1)
       |> inputs_to_ints
       |> DayOne.execute()
 
-    IO.puts("One #{day_one_part_one[:one]}")
-    IO.puts("Two #{day_one_part_one[:two]}")
+    IO.puts("1.1 #{day_one[:one]}")
+    IO.puts("1.2 #{day_one[:two]}")
   end
 
   defp inputs_to_ints(input), do: Enum.map(input, &String.to_integer(&1))
+
+  defp file_to_inputs(day) do
+    File.read!("lib/inputs/#{day}.txt")
+    |> String.trim()
+    |> String.split("\n")
+  end
 end
