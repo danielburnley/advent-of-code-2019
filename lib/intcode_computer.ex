941f9aedc9029a96feb81b622d18837f09c8a632
Daniel Burnley <dan@danops.uk>
2019-12-06 08:42:32 +0000

    Refactor intcode in preparation for mutiple length inputs

--- lib/intcode_computer.ex
+++ lib/intcode_computer.ex
@@ -3,22 +3,29 @@ defmodule IntcodeComputer do
   def run_program(program, _, :halt), do: program[0]
 
   def run_program(program, index, _status) do
-    instructions = get_next_instructions(program, index)
-    {status, program} = execute_instructions(instructions, program)
+    instruction = get_next_instruction(program, index)
+    parameters = get_instruction_parameters(program, index, instruction)
+    {status, program} = execute_instruction(instruction, parameters, program)
     run_program(program, index + 4, status)
   end
 
-  defp execute_instructions({1, first, second, result}, program) do
+  defp execute_instruction(1, {first, second, result}, program) do
     program = %{program | result => program[first] + program[second]}
     {:ok, program}
   end
 
-  defp execute_instructions({2, first, second, result}, program) do
+  defp execute_instruction(2, {first, second, result}, program) do
     program = %{program | result => program[first] * program[second]}
     {:ok, program}
   end
+  
+  defp execute_instruction(99, _, program), do: {:halt, program}
 
-  defp execute_instructions({99, _, _, _}, program), do: {:halt, program}
+  defp get_next_instruction(program, index), do: program[index]
+
+  defp get_instruction_parameters(program, index, 3), do: program[index+1]
+  defp get_instruction_parameters(program, index, 4), do: program[index+1]
+  defp get_instruction_parameters(program, index, _), do: { program[index+1], program[index+2], program[index+3] }
 
   defp get_next_instructions(program, index) do
     {program[index], program[index + 1], program[index + 2], program[index + 3]}
