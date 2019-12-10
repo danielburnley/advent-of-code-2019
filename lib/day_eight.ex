8c7981f0a9864ab3c8567720533d1962493a061a
Daniel Burnley <dan@danops.uk>
2019-12-10 09:43:53 +0000

    8.2

--- lib/day_eight.ex
+++ lib/day_eight.ex
@@ -1,7 +1,8 @@
 defmodule DayEight do
   def execute(input) do
     part_one = SpaceImageFormat.calculate_checksum(input, 25, 6)
-    [{:one, part_one}, {:two, "Net yet"}]
+    part_two = SpaceImageFormat.calculate_image(input, 25, 6)
+    [{:one, part_one}, {:two, "\n#{part_two}"}]
   end
 
 end
--- lib/programs/space_image_format.ex
+++ lib/programs/space_image_format.ex
@@ -3,8 +3,42 @@ defmodule SpaceImageFormat do
     split_input_to_layers(image, width, height) |> SpaceImageFormat.Checksum.calculate 
   end
 
-  defp split_input_to_layers(input, width, height) do
+  def calculate_image(image, width, height) do
+    split_input_to_layers(image, width, height) |> 
+    SpaceImageFormat.GenerateImage.build_image_from_layers(width, height)
+  end
+
+  def split_input_to_layers(input, width, height) do
     Enum.chunk_every(input, width) |> Enum.chunk_every(height)
+  end
+
+  defmodule GenerateImage do
+    def build_image_from_layers(layers, width, height) do
+      x_coords = for x <- 0..width-1, do: x
+      y_coords = for y <- 0..height-1, do: y
+
+      Enum.map(y_coords, fn y ->
+        Enum.map(x_coords, fn x -> 
+          get_pixel_at(layers, {x, y})
+        end)
+      end) 
+      |> Enum.map(fn row -> 
+        Enum.map(row, fn num -> 
+          display_integer(num) 
+        end) 
+      end)
+      |> Enum.map(&(Enum.join(&1, "")))
+      |> Enum.join("\n")
+    end
+
+    defp get_pixel_at(layers, {x, y}) do
+      Enum.map(layers, fn layer -> Enum.at(layer, y) |> Enum.at(x) end)
+      |> Enum.find(2, fn num -> num == 0 or num == 1 end)
+    end
+
+    defp display_integer(0), do: " "
+    defp display_integer(1), do: "X"
+    defp display_integer(2), do: " "
   end
 
   defmodule Checksum do
