3f63e3d5a775c7e31c62ce38a4a0094f94dd8de8
Daniel Burnley <dan@danops.uk>
2019-12-22 11:39:44 +0000

    13.1

--- lib/day_thirteen.ex
+++ lib/day_thirteen.ex
@@ -6,8 +6,36 @@ defmodule DayThirteen do
   end
 
   defp part_one(input) do
+    screen = spawn_link(fn -> DayThirteen.Screen.run end)
+    output_buffer = spawn_link(fn -> output_buffer(screen) end)
+
     inputter = fn -> "1\n" end
-    outputter = fn output -> IO.puts(output) end
+    outputter = fn output -> send(output_buffer, {:output, output}) end
     IntcodeComputer.run_program(input, {inputter, outputter})
+
+    send(screen, {:print, self()})
+  end
+
+  defmodule Screen do
+    def run(pixels \\ %{}) do
+      receive do
+        {:display, {x, y, id}} ->
+          run(Map.put(pixels, {x, y}, id))
+        {:print, caller} -> IO.puts(Map.values(pixels) |> Enum.count(fn x -> x == 2 end))
+      end
+    end
+  end
+
+  defp output_buffer(screen, buffer \\ [])
+  defp output_buffer(screen, [id,y,x]) do
+    send(screen, {:display, {x, y, id}})
+
+    output_buffer(screen, [])
+  end
+
+  defp output_buffer(screen, buffer) do
+    receive do
+      {:output, value} -> output_buffer(screen, [value|buffer])
+    end
   end
 end
